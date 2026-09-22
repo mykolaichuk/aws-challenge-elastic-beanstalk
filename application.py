2f63b61c591c105dd70981ec878d5d30cbda88df
Vadym Mykolaichuk <mrvadym8@gmail.com>
2026-09-22 21:56:59 +0300

    change pipeline

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "Hello from ${custom_identifier} Elastic Beanstalk CI-CD"
+    return "Hello from p2vf32dx Elastic Beanstalk CI-CD"
